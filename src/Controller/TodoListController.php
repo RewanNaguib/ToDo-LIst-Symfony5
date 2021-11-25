a31aa69b785955c67ea8e83e38a836a8788739be
Rewan Naguib <rewan.mnaguib@gmail.com>
2021-11-25 17:36:00 +0200

    adding a new task and listing all the tasks in the todo list

--- src/Controller/TodoListController.php
+++ src/Controller/TodoListController.php
@@ -2,7 +2,8 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Task;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,22 +11,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class TodoListController extends AbstractController
 {
     /**
-     * @Route("/gettodolist", name="add_task")
+     * @Route("/todolist", name="getall_tasks")
      */
     public function index()
     {
-        return $this->render('todo_list/index.html.twig');
-        // exit('this get all tasks');
+        $tasks = $this->getDoctrine()->getRepository(Task::class)->findBy([],["id"=>"DESC"]);
+        return $this->render('todo_list/index.html.twig',['tasks'=>$tasks]);
+        
     }
 
 
     /**
      * @Route("/createtodolist", name="create_task" , methods="POST")
      */
-    public function create()
+    public function create(Request $request)
     {
-        // return $this->render('todo_list/index.html.twig');
-        exit('this create new task');
+      $title = trim($request->request->get("task-title"));
+
+      if(empty($title))
+        {  
+            return $this->redirectToRoute('getall_tasks');
+        }
+ 
+
+    else 
+        {
+            $entityManager = $this->getDoctrine()->getManager();
+            $task = new Task();
+            $task->setTitle($title);
+            $entityManager->persist($task);
+            $entityManager->flush();
+            return $this->redirectToRoute('getall_tasks');
+        }
+      
     }
 
 
