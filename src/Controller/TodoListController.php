4e22b54b906068b84c14c55b159b4f196564f0cb
Rewan Naguib <rewan.mnaguib@gmail.com>
2021-11-25 15:34:59 +0200

    adding controller methods

--- src/Controller/TodoListController.php
+++ src/Controller/TodoListController.php
@@ -2,17 +2,50 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TodoListController extends AbstractController
 {
     /**
-     * @Route("/todolist", name="todo_list")
+     * @Route("/gettodolist", name="add_task")
      */
-    public function index(): Response
+    public function index()
     {
         return $this->render('todo_list/index.html.twig');
+        // exit('this get all tasks');
+    }
+
+
+    /**
+     * @Route("/createtodolist", name="create_task" , methods="POST")
+     */
+    public function create()
+    {
+        // return $this->render('todo_list/index.html.twig');
+        exit('this create new task');
+    }
+
+
+    /**
+     * @Route("/edittodolist/{id}", name="edit_task")
+     */
+    public function edit($id)
+    {
+        // return $this->render('todo_list/index.html.twig');
+        exit('this edit task' . $id);
+    }
+
+
+
+    /**
+     * @Route("/deletetodolist/{id}", name="delete_task")
+     */
+    public function delete($id)
+    {
+        // return $this->render('todo_list/index.html.twig');
+        exit('this delete task' .$id);
     }
 }
